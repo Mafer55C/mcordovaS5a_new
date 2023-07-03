27b2972f649389b387a514112e07f5233fee5f3b
master <master@SMLAB310PC02_23>
2023-07-03 18:46:39 -0500

    Agregar archivos de proyecto.

---
+++ mcordovaS5a/mcordovaS5a/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace mcordovaS5a
+{
+   
+    public partial class MainPage : ContentPage
+    {
+        private string Url = "http://192.168.20.38:8088/ws_uisrael/post.php";
+        private HttpClient cliente = new HttpClient();
+        private ObservableCollection<Estudiante> post;
+        public MainPage()
+        {
+            InitializeComponent();
+            ObtenerDatos();
+        }
+        
+        private async void ObtenerDatos()
+        {
+            var contenido = await cliente.GetStringAsync(Url);
+            List<Estudiante> listaPost = JsonConvert.DeserializeObject<List<Estudiante>>(contenido);
+            post = new ObservableCollection<Estudiante>(listaPost);
+            listaEstudiantes.ItemsSource = post;
+        }
+
+        private void btnMostrar_Clicked(object sender, EventArgs e)
+        {
+           Navigation.PushAsync(new Insertar());
+        }
+    }
+}
